4849166add6ebaa6e13a6e198d4ace8aaa9d4c7f
JansonHai <1609097487@qq.com>
2016-06-28 20:59:22 +0800

    【开发】lua逻辑框架开发

--- lua/module/bootstrap.lua
+++ lua/module/bootstrap.lua
@@ -1,22 +1,22 @@
 local filetools = require("lib/fileTools");
-local bootstrap = {};
+local tb = {};
 
-local function bootstrap(path, boot)
+function tb.bootstrap(path, isboot)
 	print(path);
 	local subdirs = filetools.get_dirs(path);
 	if (0 ~= #subdirs) then
 		for _,dirname in ipairs(subdirs) do
-			bootstrap(dirname, true);
+			tb.bootstrap(dirname, true);
 		end
 	end
 	local luaFile = path .. "/bootstrap.lua";
-	if (true == boot and filetools.is_file_exist(luaFile)) then
+	if (true == isboot and filetools.is_file_exist(luaFile)) then
 		dofile(luaFile);
 	end
 end
 
-function bootstrap.init()
-	bootstrap(LUA_SCRIPT_ROOT .. "/module", false);
+function tb.init()
+	tb.bootstrap(LUA_SCRIPT_ROOT .. "/module", false);
 end
 
-return bootstrap;
+return tb;
